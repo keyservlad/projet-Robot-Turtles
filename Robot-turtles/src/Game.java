dc94aab3d5440193b69bd91671974d6e542a26ef
keyservlad <58270823+keyservlad@users.noreply.github.com>
2019-11-29 16:17:22 +0100

    initialisation du plateau et affichage

--- Robot-turtles/src/Game.java
+++ Robot-turtles/src/Game.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 
 public class Game {
 	
-	public int nbTortues;
-	public List tortues = new ArrayList<String>();
+	public static int nbTortues;
+	public List tortues = new ArrayList<>();
 	public Scanner scanner = new Scanner(System.in);
 	
 	
 	public void run() {
 		choixNbJoueur();
 		Plateau plateau = new Plateau();
-		plateau.initialisationPlateau(nbTortues);
+		
+		plateau.afficherTableau();
+		
+		
+		
+		
+		
 		
 		
 	}
@@ -24,26 +30,26 @@ public class Game {
 			System.out.println("Combien de joueurs (entre 2 et 4 joueurs)?");
 			saisieNbJoueurs = Integer.parseInt(scanner.nextLine());
 			
-		}while(saisieNbJoueurs != 2 || saisieNbJoueurs != 3 || saisieNbJoueurs != 4);
+		}while(saisieNbJoueurs != 2 && saisieNbJoueurs != 3 && saisieNbJoueurs != 4);
 		
 		
 		this.nbTortues = saisieNbJoueurs;
 		
 		switch(nbTortues) {
 		case 2:
-			tortues.add("Rouge");
-			tortues.add("Vert");
+			tortues.add('R');
+			tortues.add('V');
 			break;
 		case 3:
-			tortues.add("Rouge");
-			tortues.add("Vert");
-			tortues.add("Bleu");
+			tortues.add('R');
+			tortues.add('V');
+			tortues.add('B');
 			break;
 		case 4:
-			tortues.add("Rouge");
-			tortues.add("Vert");
-			tortues.add("Bleu");
-			tortues.add("Jaune");
+			tortues.add('R');
+			tortues.add('V');
+			tortues.add('B');
+			tortues.add('J');
 		}
 	}
 
--- Robot-turtles/src/Plateau.java
+++ Robot-turtles/src/Plateau.java
@@ -5,7 +5,15 @@ public class Plateau {
 	
 	
 	
+	public Plateau() {
+		initialisationPlateau(Game.nbTortues);
+	}
+	
+	
 	public void initialisationPlateau(int nbTortues) {
+		
+		
+		
 		for (int i = 0; i < plateau.length; i++) {
             for (int j = 0; j < plateau[0].length; j++) {
                 plateau[i][j] = ' ';
@@ -14,17 +22,62 @@ public class Plateau {
 		// rajouter les tortues et les joyaux et les tuiles
 		switch(nbTortues) {
 		case 2:
+			plateau[0][1] = 'R';
+			plateau[0][5] = 'V';
+			
+			plateau[7][3] = 'X';
+			
+			for (int i = 0; i < 8; i++) {
+				plateau[i][7] = 'M';
+			}
 			
 			break;
 			
 		case 3:
+			
+			plateau[0][0] = 'R';
+			plateau[0][3] = 'V';
+			plateau[0][6] = 'B';
+			
+			plateau[7][0] = 'X';
+			plateau[7][3] = 'X';
+			plateau[7][6] = 'X';
+			
+			
+			for (int i = 0; i < 8; i++) {
+				plateau[i][7] = 'M';
+			}
 			break;
 			
 		case 4:
+			
+			plateau[0][0] = 'R';
+			plateau[0][2] = 'V';
+			plateau[0][5] = 'B';
+			plateau[0][7] = 'J';
+			
+			
+			plateau[7][1] = 'X';
+			plateau[7][6] = 'X';
+			
+			
 			break;
 		
 		}
 		
+		
+		
+	}
+	
+	public void afficherTableau() {
+		
+		for (int i = 0; i < plateau.length; i++) {
+            System.out.print("|");
+            for (int j = 0; j < plateau[i].length; j++) {
+                System.out.print(plateau[i][j]);
+            }
+            System.out.println("|");
+        }
 		
 	}
 
